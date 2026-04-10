3687dd0fec656733f58387a92ff5937c610f3fd7
zerogex-options <zerogexoptions@gmail.com>
2026-04-10 15:29:27 -0400

    Add configurable canonical flow backfill window

--- tests/test_api_database_flow_refresh.py
+++ tests/test_api_database_flow_refresh.py
@@ -7,7 +7,7 @@ from src.api.database import DatabaseManager
 
 class _FakeFlowRefreshConn:
     def __init__(self):
-        self.execute_queries = []
+        self.execute_calls = []
 
     async def fetchval(self, query, *_args):
         if "FROM option_chains" in query:
@@ -15,11 +15,11 @@ class _FakeFlowRefreshConn:
         if "FROM underlying_quotes" in query:
             return 5200.0
         if "FROM flow_contract_facts" in query:
-            return datetime(2026, 4, 10, 13, 55, tzinfo=timezone.utc)
+            return None
         return None
 
     async def execute(self, query, *_args):
-        self.execute_queries.append(query)
+        self.execute_calls.append((query, _args))
         return "INSERT 0 1"
 
 
@@ -27,11 +27,16 @@ def test_refresh_flow_cache_seeds_prior_rows_for_lag():
     db = DatabaseManager()
     conn = _FakeFlowRefreshConn()
     os.environ["FLOW_CANONICAL_ONLY"] = "true"
+    os.environ["FLOW_CANONICAL_BACKFILL_MINUTES"] = "240"
 
     asyncio.run(db._do_refresh_flow_cache(conn, "SPX"))
 
-    assert conn.execute_queries
-    canonical_query = next((q for q in conn.execute_queries if "INSERT INTO flow_contract_facts" in q), "")
+    assert conn.execute_calls
+    canonical_call = next(
+        ((q, args) for (q, args) in conn.execute_calls if "INSERT INTO flow_contract_facts" in q),
+        ("", ()),
+    )
+    canonical_query, canonical_args = canonical_call
     assert canonical_query
     assert "WITH window_rows AS (" in canonical_query
     assert "seed_rows AS (" in canonical_query
@@ -40,3 +45,5 @@ def test_refresh_flow_cache_seeds_prior_rows_for_lag():
     assert "source_rows AS (" in canonical_query
     assert "FROM source_rows s" in canonical_query
     assert "WHERE timestamp >= $2" in canonical_query
+    # backfill_start should honor FLOW_CANONICAL_BACKFILL_MINUTES
+    assert canonical_args[1] == datetime(2026, 4, 10, 10, 0, tzinfo=timezone.utc)
